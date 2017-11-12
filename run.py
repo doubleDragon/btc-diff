55bc623fcb32e65c23dcf8b9faa44ad7e4aa6d00
doubleDragon <aiai373824745@gmail.com>
2017-11-12 17:37:31 +0800

    Debug success

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# from app import app
-#
-# app.run(host='0.0.0.0')
-
 from app import create_app
 
+app = create_app()
 if __name__ == '__main__':
-    app = create_app()
-
     app.run(host='0.0.0.0')
     # app.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#from project import app
+from run import app
+
+if __name__ == "__main__":
+    app.run()
