c0441caf76e8c3d7d35222f00a30f8af94ad4e7e
doubleDragon <aiai373824745@gmail.com>
2017-11-12 16:49:10 +0800

    Change structure

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from app import app
+# from app import app
+#
+# app.run(host='0.0.0.0')
 
-app.run(debug=True)
+from app import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+
+    app.run(host='0.0.0.0')
+    # app.run(debug=True)
